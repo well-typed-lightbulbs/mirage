daf64df92400e8ddfc140dfdf9aa9cf1b5498898
Anil Madhavapeddy <anil@recoil.org>
2012-05-22 22:46:13 +0100

    [tcp] add a Tcp.Wire module to handle get/set of various fields

---
+++ lib/net/direct/tcp/wire.ml
@@ -0,0 +1,55 @@
+(*
+ * Copyright (c) 2012 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+cstruct tcpv4 {
+  uint16_t src_port;
+  uint16_t dst_port;
+  uint32_t sequence;
+  uint32_t ack_number;
+  uint16_t dataoff_flags;
+  uint16_t window;
+  uint16_t checksum;
+  uint16_t urg_ptr
+} as big_endian
+
+open Cstruct
+
+let get_data_offset buf = ((get_uint8 buf 12) lsr 4) * 4
+
+let get_fin buf = ((get_uint8 buf 13) land (1 lsl 0)) > 0
+let get_syn buf = ((get_uint8 buf 13) land (1 lsl 1)) > 0
+let get_rst buf = ((get_uint8 buf 13) land (1 lsl 2)) > 0
+let get_psh buf = ((get_uint8 buf 13) land (1 lsl 3)) > 0
+let get_ack buf = ((get_uint8 buf 13) land (1 lsl 4)) > 0
+let get_urg buf = ((get_uint8 buf 13) land (1 lsl 5)) > 0
+let get_ece buf = ((get_uint8 buf 13) land (1 lsl 6)) > 0
+let get_cwr buf = ((get_uint8 buf 13) land (1 lsl 7)) > 0
+
+let set_fin buf = set_uint8 buf 13 ((get_uint8 buf 13) lor (1 lsl 0))
+let set_syn buf = set_uint8 buf 13 ((get_uint8 buf 13) lor (1 lsl 1))
+let set_rst buf = set_uint8 buf 13 ((get_uint8 buf 13) lor (1 lsl 2))
+let set_psh buf = set_uint8 buf 13 ((get_uint8 buf 13) lor (1 lsl 3))
+let set_ack buf = set_uint8 buf 13 ((get_uint8 buf 13) lor (1 lsl 4))
+let set_urg buf = set_uint8 buf 13 ((get_uint8 buf 13) lor (1 lsl 5))
+let set_ece buf = set_uint8 buf 13 ((get_uint8 buf 13) lor (1 lsl 6))
+let set_cwr buf = set_uint8 buf 13 ((get_uint8 buf 13) lor (1 lsl 7))
+
+let get_options buf =
+  if get_data_offset buf > 20 then
+    Options.unmarshal (shift buf sizeof_tcpv4) else []
+
+let set_options buf ts =
+  Options.marshal buf ts
