26de8b25d990130834014f48921749487a637934
Anil Madhavapeddy <anil@recoil.org>
2012-06-01 16:02:17 +0100

    [net] update Sliding_window with bind functions so we can recalculate dependent variables

--- lib/net/direct/tcp/sliding_window.ml
+++ lib/net/direct/tcp/sliding_window.ml
@@ -18,36 +18,91 @@ open Printf
 
 (* Type of sequence number. TODO functorize *)
 type seq = Sequence.t
+type seq_waiters = unit Lwt.u Lwt_sequence.t
 
-(* An edge only increments and can have waiters for certain
- * conditions to be satisfied on it *)
-module Edge = struct
-  type t = {
-    mutable seq: seq;
-  }
-
-  let create ~seq = { seq }
-  let seq t = t.seq
-end
-  
 (* Window advances left to right only. No going backwards! *)
 type t = {
-  l: Edge.t;
-  m: Edge.t; 
-  r: Edge.t;
+  mutable l: seq;
+  mutable m: seq;
+  mutable r: seq;
+  mutable l_dirty: bool;
+  mutable m_dirty: bool;
+  mutable r_dirty: bool;
+  lm_w: seq_waiters;
+  mr_w: seq_waiters;
 }
 
 (* Initialise a new window with initial [isn] and maximum size [mss] *)
 let create ~isn ~mss =
-  let l = Edge.create ~seq:isn in
-  let m = Edge.create ~seq:isn in
-  let r = Edge.create ~seq:(Sequence.add isn mss) in
-  { l; m; r }
+  let l = isn in
+  let m = isn in
+  let r = Sequence.add isn mss in
+  let l_dirty = false in
+  let m_dirty = false in
+  let r_dirty = false in
+  let lm_w = Lwt_sequence.create () in
+  let mr_w = Lwt_sequence.create () in
+  { l; m; r; l_dirty; m_dirty; r_dirty; lm_w; mr_w }
 
 (* Test if a sequence number is valid for a window *)
-let valid ~seq t =
-  let lseq = Edge.seq t.l in
-  let rseq = Edge.seq t.r in
-  printf "SW.valid %s < %s < %s\n" (Sequence.to_string lseq)
-    (Sequence.to_string seq) (Sequence.to_string rseq);
-  Sequence.between seq (Edge.seq t.l) (Edge.seq t.r)
+let valid t seq =
+  printf "SW.valid %s <= %s <= %s\n" (Sequence.to_string t.l)
+    (Sequence.to_string seq) (Sequence.to_string t.r);
+  Sequence.between seq t.l t.r
+
+let get_l t = t.l
+let get_m t = t.m
+let get_r t = t.r
+
+(* TODO enforce l <= m <= r in these additions *)
+let add_l t seq =
+  printf "SW.incr_l %s + %s\n" (Sequence.to_string t.l) (Sequence.to_string seq);
+  t.l <- Sequence.add t.l seq;
+  t.l_dirty <- true
+
+let add_m t seq =
+  printf "SW.incr_m %s + %s\n" (Sequence.to_string t.m) (Sequence.to_string seq);
+  t.m <- Sequence.add t.m seq;
+  t.m_dirty <- true
+
+let add_r t seq =
+  printf "SW.incr_r %s + %s\n" (Sequence.to_string t.r) (Sequence.to_string seq);
+  t.r <- Sequence.add t.r seq;
+  t.r_dirty <- true
+
+let wait_lm t =
+  let th,u = Lwt.task () in
+  let node = Lwt_sequence.add_r u t.lm_w in
+  Lwt.on_cancel th (fun () -> Lwt_sequence.remove node);
+  th
+
+let wait_mr t =
+  let th,u = Lwt.task () in
+  let node = Lwt_sequence.add_r u t.mr_w in
+  Lwt.on_cancel th (fun () -> Lwt_sequence.remove node);
+  th
+
+let rec wakeup_all w =
+  match Lwt_sequence.take_opt_l w with
+  |None -> ()
+  |Some u -> Lwt.wakeup u (); wakeup_all w
+
+(* Wakeup any bound waiters to a variable *)
+let wakeup t =
+  match t.m_dirty with
+  |false -> ()
+  |true ->
+     if t.l_dirty then wakeup_all t.lm_w;
+     if t.r_dirty then wakeup_all t.mr_w
+
+(* Bind a function that is recalculated every time the
+ * values of the edges change *)
+let rec bind_lm fn t =
+  let () = fn t.l t.m in
+  lwt () = wait_lm t in
+  bind_lm fn t
+
+let rec bind_mr fn t =
+  let () = fn t.l t.m in
+  lwt () = wait_mr t in
+  bind_mr fn t
